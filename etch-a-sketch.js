8da7dd6b275c94681f8275ced89b974b7ccc3680
Alperen Mimaroglu <alperenmimaroglu@gmail.com>
2023-07-14 21:29:45 +0200

    Adding row for items

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,9 +1,19 @@
 const container = document.querySelector(".container");
 console.log(container)
 
-const gridAmount = 256;
+const width = 1024;
+container.style.width = `${width}px`;
+container.style.height = `${width}px`;
+
+const gridAmount = 8;
+
 for (let i = 0; i < gridAmount; i++) {
-    const item = document.createElement('div');
-    item.className = 'item';
-    container.appendChild(item);
+    const row = document.createElement('div');
+    row.className = 'row';
+    container.appendChild(row);
+    for (let j = 0; j < gridAmount; j++) {
+        const item = document.createElement('div');
+        item.className = 'item';
+        row.appendChild(item);
+    }
 }
